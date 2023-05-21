c44f4e9380329caf1fe9d1865d38ab1a057b7df6
Isachenko Ivan <vanisachenko@gmail.com>
2023-05-21 16:40:09 +0600

    First steps

---
+++ app/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.encoders import jsonable_encoder
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=
+    [
+
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get('/analyze')
+def analyze_simple():
+    return{'result': [
+        {'Адрес': 'Красковская ул., д.121А', 'Наименование работы': 'Ремонт освещения'},
+        {'Адрес': 'Владимирская ул., д.12', 'Наименование работы': 'Замена лестницы'},
+        {'Адрес': 'Кусковская ул., д.1', 'Наименование работы': 'Замена крыльца'},
+        {'Адрес': 'Бойцовая ул., д.11', 'Наименование работы': 'Сбивание сосулек'}
+    ]}
+
